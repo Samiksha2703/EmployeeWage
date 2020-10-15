52133d86b0008e7e95eda0e47d10456638915ca6
samiksha Shende <shende.samiksha@rediffmail.com>
2020-10-15 19:56:29 +0530

    Added : Employee Wage Computation Program with Java

---
+++ src/com/employeeWageforMultipleCompanies/InterfaceEmployeeWage.java
@@ -0,0 +1,8 @@
+package com.employeeWageforMultipleCompanies;
+
+public interface InterfaceEmployeeWage {
+
+	public void addCompanyEmpWage(String company, double wagePerHour, int numberofWorkingDays, int maxHoursInMonth);
+	
+	public void computeEmpWage();
+}
